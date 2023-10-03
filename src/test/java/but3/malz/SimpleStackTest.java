b7a0d6d2ce3db5ebe328cbf81c336a5a93c60b9c
Mathys ALZURIA <mathysalzuria@gmail.com>
2023-10-03 17:44:51 +0200

    commit fin de séance 03/10/2023

---
+++ src/test/java/but3/malz/SimpleStackTest.java
@@ -0,0 +1,32 @@
+package but3.malz;
+
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+
+public class SimpleStackTest {
+
+    @Test
+    @DisplayName("testIsEmpty")
+    void isEmpty() {
+    }
+
+    @Test
+    @DisplayName("testGetSize")
+    void getSize() {
+    }
+
+    @Test
+    @DisplayName("testPush")
+    void push() {
+    }
+
+    @Test
+    @DisplayName("testPeek")
+    void peek() {
+    }
+
+    @Test
+    @DisplayName("testPop")
+    void pop() {
+    }
+}
